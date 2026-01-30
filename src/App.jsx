c1634ca8bdcab6d2f62b3f0b636e9e5de53e913e
Abbas Manzoor <aabbasmanzoor101@gmail.com>
2026-01-30 07:02:55 +0500

    practice function calling

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+const App = () => {
+  const pageScrolling = (elem) => {
+  if(elem>0){
+   console.log('down')
+  } else {
+   console.log('up')
+  }
+}
+  return (
+    <div onWheel={(elem)=>{
+      pageScrolling(elem.deltaY);
+    }}>
+      <div className='page1'></div>
+        <div className='page2'></div>
+          <div className='page3'></div>
+
+</div>
+  )}
+export default App
