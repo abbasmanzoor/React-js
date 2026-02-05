e0758126bd4d6fe834331a93eae23645ec740e4a
Abbas Manzoor <aabbasmanzoor101@gmail.com>
2026-02-05 07:09:25 +0500

    Two way handling  practice

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [title,settitle]=useState('');
+
+  const submithandler=(e)=>{
+    e.preventDefault()
+  console.log("Submitted", title);
+  settitle('');
+  }
+  return (
+    <div>
+      <form onSubmit={(e)=>{
+        submithandler(e)
+      }}>
+        <input type="text"  placeholder="Username"
+        value={(title)} 
+        onChange={(e)=>{
+          settitle(e.target.value)
+        }}/>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
